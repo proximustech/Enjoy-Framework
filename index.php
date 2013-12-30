f9513da703f0e7fb8cc8f450fd1475897da21800
julianariasquijano <horsarias@hotmail.com>
2013-12-30 09:33:03 -0500

    handling none existent views

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-//Application Controller
 
-require_once "applications/appServerConfig.php"; //Expose variable $config
+/*
+ * General Application Server Controller
+ */
+
+
+//Set initial defaults
+require_once "applications/appServerConfig.php"; //Expose variable $appServerConfig
 
 $app=$appServerConfig["base"]["defaultApp"];
 if (key_exists("app", $_GET)) {
@@ -16,7 +21,6 @@ require_once "applications/$app/config.php"; //Expose variable $config
 
 $mod=$config["base"]["defaultModule"];
 $act=$config["base"]["defaultAction"];
-
 
 if (key_exists("mod", $_GET)) {
     $mod=$_GET["mod"];
@@ -30,7 +34,6 @@ $config["flow"]["mod"]=$mod;
 $config["flow"]["act"]=$act;
 $language=$config["base"]["language"];
 
-require_once 'dataRep/appServer_dataRep.php';
 
 $modelsDir="applications/$app/modules/$mod/models/";
 $viewsDir="applications/$app/modules/$mod/views/";
@@ -40,29 +43,42 @@ $applicationsServerDataRepDir="dataRep/";
 $applicationDataRepDir="applications/$app/dataRep/";
 $moduleDataRepDir="applications/$app/modules/$mod/dataRep/";
 
+
+require_once 'dataRep/appServer_dataRep.php';
+
 //$directories["models"]=$modelsDir;
 //$directories["views"]=$viewsDir;
 //$directories["controllers"]=$controllersDir;
 //$directories["applicationDataRepDir"]=$applicationDataRepDir;
 
+
+//Application Controller Execution
 require_once $controllersDir."controller_$mod.php";
 
 $modController = new modController($config);
 //$modController->directories=$directories;
 $modController->run($act);
 
-//Expose the data Output Array as variables for the view
+//Data result handing
+$view=$modController->resultData["view"];
+$viewFile=$viewsDir."view_$view.php";
 
-if (key_exists("output", $modController->resultData)) {
-    
-    foreach ($modController->resultData["output"] as $outputVarName =>$outputVarValue) {
-        //Although $outputVarValue is not used, it is necesary for the correct use of $outputVarName
-        $$outputVarName = &$modController->resultData["output"][$outputVarName];
+if (file_exists($viewFile)) {
+    //Expose the data Output Array as variables for the view
+    if (key_exists("output", $modController->resultData)) {
+
+        foreach ($modController->resultData["output"] as $outputVarName =>$outputVarValue) {
+            //Although $outputVarValue is not used, it is necesary for the correct use of $outputVarName
+            $$outputVarName = &$modController->resultData["output"][$outputVarName];
+        }
+
     }
     
+    require_once $viewFile;
+}
+else if(key_exists("output", $modController->resultData)){
+    echo $modController->resultData["output"];
 }
 
-$view=$modController->resultData["view"];
-require_once $viewsDir."view_$view.php";
 
 ?>
