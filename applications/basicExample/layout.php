73cbaf08e693c3cbe9f2177fd9fdef3e723b3a38
Julian Arias <horsarias@hotmail.com>
2015-03-09 16:53:39 -0500

    reestructured layout system

--- applications/basicExample/layout.php
+++ applications/basicExample/layout.php
@@ -1,42 +1,52 @@
 <html>
 
-<script src="assets/js/enjoy/e_common.js"></script>
-<link href="assets/css/enjoy/helpers.css" rel="stylesheet">
+    <head>
+        <script src="assets/js/enjoy/e_common.js"></script>
+        <link href="assets/css/enjoy/helpers.css" rel="stylesheet">
 
-<script src="assets/js/jquery/jquery-1.7.1.min.js"></script>
-<!--<script src="assets/js/jquery/plugins/kendoui/js/jquery.min.js"></script>-->
+        <script src="assets/js/jquery/jquery-1.7.1.min.js"></script>
+        <!--<script src="assets/js/jquery/plugins/kendoui/js/jquery.min.js"></script>-->
 
-<link href="assets/js/jquery/plugins/bootstrap/bootstrap.min.css" rel="stylesheet">
-<link href="assets/js/jquery/plugins/bootstrap/bootstrap-theme.min.css" rel="stylesheet">
-<script src="assets/js/jquery/plugins/bootstrap/bootstrap.min.js"></script>
+        <!--
+        <link href="assets/js/jquery/plugins/bootstrap/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/js/jquery/plugins/bootstrap/bootstrap-theme.min.css" rel="stylesheet">
+        <script src="assets/js/jquery/plugins/bootstrap/bootstrap.min.js"></script>
+        -->
 
-<!--jqueryUi Set-->
-<!--<script src="assets/js/jquery/ui/jquery-ui.js"></script>-->
-<!--<link rel="stylesheet" href="assets/js/jquery/themes/redmond/jquery-ui.css" />-->
+        <!--jqueryUi Set-->
+        <!--<script src="assets/js/jquery/ui/jquery-ui.js"></script>-->
+        <!--<link rel="stylesheet" href="assets/js/jquery/themes/redmond/jquery-ui.css" />-->
 
-<script src="assets/js/jquery/plugins/jqueryui/jquery-ui.min.js"></script>
-<link rel="stylesheet" href="assets/js/jquery/plugins/jqueryui/jquery-ui.css" />
+        <script src="assets/js/jquery/plugins/jqueryui/jquery-ui.min.js"></script>
+        <link rel="stylesheet" href="assets/js/jquery/plugins/jqueryui/jquery-ui.css" />
 
-<script src="assets/js/jquery/plugins/timepicker/jquery-ui-timepicker-addon.js"></script>
-<script src="assets/js/jquery/plugins/timepicker/jquery-ui-sliderAccess.js"></script>
-<script src="assets/js/jquery/plugins/timepicker/jquery-ui-timepicker-addon.css"></script>
-<script src="assets/js/jquery/plugins/combobox/combobox.js"></script>
-<link rel="stylesheet" href="assets/js/jquery/plugins/combobox/combobox.css">
+        <script src="assets/js/jquery/plugins/timepicker/jquery-ui-timepicker-addon.js"></script>
+        <script src="assets/js/jquery/plugins/timepicker/jquery-ui-sliderAccess.js"></script>
+        <script src="assets/js/jquery/plugins/timepicker/jquery-ui-timepicker-addon.css"></script>
 
+        <script src="assets/js/jquery/plugins/combobox/combobox.js"></script>
+        <link rel="stylesheet" href="assets/js/jquery/plugins/combobox/combobox.css">
 
-<!--KendoUi Set-->
-
-<link rel="stylesheet" href="assets/js/jquery/plugins/kendoui/styles/kendo.common.min.css" />
-<link rel="stylesheet" href="assets/js/jquery/plugins/kendoui/styles/kendo.default.min.css" />
-
-<script src="assets/js/jquery/plugins/kendoui/js/kendo.core.min.js"></script>
-<script src="assets/js/jquery/plugins/kendoui/js/kendo.ui.core.min.js"></script>
+        <link type="text/css" href="assets/js/jquery/plugins/michael-multiselect/css/ui.multiselect.css" rel="stylesheet" />
+        <script type="text/javascript" src="assets/js/jquery/plugins/michael-multiselect/js/plugins/localisation/jquery.localisation-min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery/plugins/michael-multiselect/js/plugins/scrollTo/jquery.scrollTo-min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery/plugins/michael-multiselect/js/ui.multiselect.js"></script>
 
 
-<body>
+        <!--KendoUi Set-->
 
-<?php  require_once $viewFile;  ?>
+        <link rel="stylesheet" href="assets/js/jquery/plugins/kendoui/styles/kendo.common.min.css" />
+        <link rel="stylesheet" href="assets/js/jquery/plugins/kendoui/styles/kendo.default.min.css" />
 
-</body>
+        <script src="assets/js/jquery/plugins/kendoui/js/kendo.core.min.js"></script>
+        <script src="assets/js/jquery/plugins/kendoui/js/kendo.ui.core.min.js"></script>
+
+
+    </head>
+    <body>
+
+    <?php  require_once $viewFile;  ?>
+
+    </body>
 
 </html>
