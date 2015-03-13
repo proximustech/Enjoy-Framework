1fa974e85ffab37f2e40c352def59a12679427b1
Julian Arias <horsarias@hotmail.com>
2015-03-13 11:28:18 -0500

    adding Notes

--- applications/basicExample/modules/home/views/view_index.php
+++ applications/basicExample/modules/home/views/view_index.php
@@ -26,6 +26,7 @@ foreach ($names as $name) {
 
 
 $uiArray["grouperControl"]["selectControl"]["value"]=$selectData;
+$uiArray["grouperControl"]["z_selectControl"]["value"]=$selectData;
 
 $uiJson='
 {
@@ -34,7 +35,8 @@ $uiJson='
         "caption":"Controles",
         "expanded":"true",
         "value":{
-            "selectControl":{"name":"select1","caption":"Select:","autoComplete":"false","t_style":"width: 200px"},
+            "selectControl":{"name":"select1","caption":"Select:","autoComplete":"true"},
+            "z_selectControl":{"name":"select2","caption":"Select:","multiple":"true"},
             "xControl":{"tag":"hr"},
             "dateTimeControl":{"name":"dateTime1","caption":"Fecha y Hora:"},
             "dateControl":{"name":"time1","caption":"Fecha:"},
@@ -46,13 +48,18 @@ $uiJson='
                 "value":{
                     "checkBoxControl":{"name":"checkBox1","caption":"Chequeo:","t_checked":"","value":"alfa"},
                     "radioControl":{"name":"radio1","caption":"Radio:","t_checked":"","value":"alfa"},
-                    "efe_radioControl":{"name":"radio1","caption":"Radio B:","value":"beta"}                  
+                    "efe_radioControl":{"name":"radio1","caption":"Radio B:","value":"beta"},
+                    "fileControl":{"name":"file1","caption":"Archivo con un caption relargisisissisimo:"}
                 }
             },
             "2_xControl":{"value":"<br>"},
             "textBoxControl":{"name":"text2","caption":"Data 1","value":"Hello","t_disabled":"true"},
             "2_textBoxControl":{"name":"text1","caption":"Data 2","value":"Every body"},
-            "a_xControl":{"value":"ClickMe","tag":"button","t_onclick":"alert(\'Hola\')"}
+            "3_xControl":{"t_class":"eui_note red","t_style":"width:100px","value":"<p>Alerta</p>"},
+            "4_xControl":{"t_class":"eui_note green","t_style":"width:300px","value":"<p>Ok</p>"},
+            "5_xControl":{"t_class":"eui_note blue","t_style":"width:600px","value":"<p>Sky</p>"},
+            "6_xControl":{"t_class":"eui_note taupe","value":"<p>Check Out</p>"},
+            "a_xControl":{"value":"ClickMe","tag":"button","t_class":"eui_button","t_onclick":"alert(\'Hola\')"}
         }
     }
 }';
