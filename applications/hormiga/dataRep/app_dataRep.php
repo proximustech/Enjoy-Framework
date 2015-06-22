0e9230d086b62a3aba866070b583bc91072cc4ff
Julian Arias <horsarias@hotmail.com>
2015-06-22 08:43:10 -0500

    correcting bug

--- applications/hormiga/dataRep/app_dataRep.php
+++ applications/hormiga/dataRep/app_dataRep.php
@@ -1,15 +1,9 @@
 <?php
 
-require_once "lib/dataRepositories/interfaces.php";
+require_once "lib/dataRepositories/mysqlDataRep.php";
 
-class hormigaDataRep implements dataRep_Interface {
+class hormigaDataRep extends dataRep {
 
-    var $instance=null;
-    var $host;
-    var $dbname;
-    var $username;
-    var $password;    
-    
     function __construct() {
         
         $this->host	= "localhost";
@@ -17,39 +11,6 @@ class hormigaDataRep implements dataRep_Interface {
         $this->username = "";
         $this->password = "";        
         
-    }
-
-    function getInstance() {
-
-        $this->instance = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->username, $this->password);
-        $this->instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        return $this->instance;
-        
-    }
-    
-    function close() {
-        $this->instance = null;
-    }
-
-    public function dbExists($dataBase=null) {
-        
-        if ($dataBase == null) $dataBase=$this->dbname;
-        
-        $sql="SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$dataBase'";
-        $query = $this->dataRep->prepare($sql);
-        $query->execute();
-        $results = $query->fetchAll(PDO::FETCH_ASSOC);
-        
-        if (count($results)) {
-            return true;
-        }
-        else return false;
-        
-    }
-
-    public function __destruct() {
-        $this->close();
     }
 
 }
